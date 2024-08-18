9f612a08db5e0c055a35901f44b1c2e94108e839
MatejHor <mat.horniak@gmail.com>
2024-08-18 15:19:20 +0000

    Add portalInvoice class

---
+++ cloudinventario-invoice/en.dict.cloudinventario-invoice.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Localized data
+ *
+ * @copyright   Copyright (C) 2013 XXXXX
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+Dict::Add('EN US', 'English', 'English', array(
+    'Menu:PortalInvoice'	=> 'PortalInvoice',
+	'Menu:PortalInvoice+'	=> 'PortalInvoice',
+
+    'Menu:PortalClient'	=> 'PortalClient',
+	'Menu:PortalClient+'	=> 'PortalClient',
+
+	'PortalInvoice:baseinfo'	=> 'Basic information',
+	'PortalInvoice:addinfo'		=> 'Additional information',
+
+	'PortalClient:baseinfo'	=> 'Basic information',
+	'PortalClient:addinfo'		=> 'Additional information',
+
+	'Class:PortalInvoice'		=> 'PortalInvoice',
+	'Class:PortalInvoice/Attribute:name'	=> 'Name',
+	'Class:PortalInvoice/Attribute:description'	=> 'Description',
+	'Class:PortalInvoice/Attribute:quantity'	=> 'Quantity',
+	'Class:PortalInvoice/Attribute:unit'	=> 'Unit',
+	'Class:PortalInvoice/Attribute:unit_price'	=> 'UnitPrice',
+	'Class:PortalInvoice/Attribute:tax'	=> 'Tax',
+
+	'Class:PortalClient'		=> 'v',
+	'Class:PortalClient/Attribute:name'	=> 'Name',
+	'Class:PortalClient/Attribute:email'	=> 'Email',
+	'Class:PortalClient/Attribute:address'	=> 'Address',
+	'Class:PortalClient/Attribute:city'	=> 'City',
+	'Class:PortalClient/Attribute:zip'	=> 'Zip',
+	'Class:PortalClient/Attribute:phone'	=> 'Phone',
+));
